42a8e0b700db1aadf33cb091ac3845ebc0f08842
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2024-07-31 14:39:41 +0300

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router-dom"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <Routes>
+      <h1 className="italic">Hello React</h1>
+<Route/>
+    </Routes>
+    // <>
+    //  <h1 className='text-3xl font-bold underline'>Hello World</h1>
+    // </>
+  )
+}
+
+export default App
---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Children } from "react";
+
+
+
+
+const MainLayout = (Children: any) => {
+return <div className="container mx-auto bg-[#34d399]">
+
+{Children}
+</div>
+};
+
+export default MainLayout;
